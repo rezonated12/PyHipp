e0b6bb01e3644db31471385cf55485e1baf0f24a
AWS ParallelCluster user <ec2-user@ip-10-0-12-237.ap-southeast-1.compute.internal>
2024-09-13 05:32:09 +0000

    Update freq-slurm.sh

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -16,5 +16,5 @@
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.FreqSpectrum(saveLevel=1); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
 print(time.localtime());"
